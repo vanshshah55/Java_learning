66e92b86177e7e2ea7a247e0261cba4557fcb8ef
Vanshshah55 <vips.vansh@gmail.com>
2025-10-07 18:32:14 +0530

    Second session practice codes

---
+++ a9_ifelse.java
@@ -0,0 +1,18 @@
+public class a9_ifelse {
+    public static void main(String[] args){
+        int a = 10;
+
+        if(a>0){
+            System.out.println("a is a positive number");
+        } else {
+            System.out.println("a is not a positive number");
+        }
+
+        if (a < 10){
+            System.out.println(a);
+            System.out.println("a is less than 10");
+        } else {
+            System.out.println(a);
+            System.out.println("a is greater than 10");}
+    }
+}
