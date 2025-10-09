c58287b527316730ce25998b77fb2a8c379c0cd1
Vanshshah55 <vips.vansh@gmail.com>
2025-10-09 20:21:23 +0530

    Fourth session practice codes

---
+++ a20_array.java
@@ -0,0 +1,18 @@
+public class a20_array {
+    public static void main(String args[]){
+        int num[] = {5,6,7};
+        // System.out.println(num);
+        System.out.println(num[1]);
+
+
+        int nums[] =  new int[4];
+        nums[0] = 2;
+        nums[1] = 3;
+        nums[2] = 4;
+        nums[3] = 5;
+
+        for(int i=0;i<=nums.length;i++){
+            System.out.println((nums[i]));
+        }
+    }
+}
