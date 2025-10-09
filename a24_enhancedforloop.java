9b0a80decc1ab172d46401db77111b4be5fb97a2
Vanshshah55 <vips.vansh@gmail.com>
2025-10-10 00:48:35 +0530

    Fourth session practice codes

---
+++ a24_enhancedforloop.java
@@ -0,0 +1,43 @@
+
+
+class student{
+    int rollno;
+    String name;
+    int marks;
+}
+
+public class a24_enhancedforloop {
+
+    public static void main(String[] a){
+        student s1 = new student();
+        s1.rollno = 1;
+        s1.name = "Vansh";
+        s1.marks = 55;
+
+        student s2 = new student();
+        s2.rollno = 2;
+        s2.name = "Apollo";
+        s2.marks = 99;
+
+        student s3 = new student();
+        s3.rollno = 3;
+        s3.name = "Java";
+        s3.marks = 98;
+
+        System.out.println((+ s1.rollno + " : " + s1.name + " : " + s1.marks));
+        System.out.println((+ s2.rollno + " : " + s2.name + " : " + s2.marks));
+        System.out.println((+ s3.rollno + " : " + s3.name + " : " + s3.marks));
+
+        student studarray[] = new student[3];
+        studarray[0]=s1;
+        studarray[1]=s2;
+        studarray[2]=s3;
+
+        // ENHANCED FOR LOOP
+
+        for (student stud: studarray){
+            System.out.println(stud.name + " = " + stud.rollno);
+        }
+
+    }
+}
