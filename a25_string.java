b68e453a0032fe17703b6e8e4f6a1e4059783d34
Vanshshah55 <vips.vansh@gmail.com>
2025-10-10 01:31:28 +0530

    Fourth session practice codes

--- a25_string.java
+++ a25_string.java
@@ -1,11 +1,21 @@
 public class a25_string {
     public static void main(String[] a){
-        String name = new String();
-        name = "Vansh";
-        System.out.println(name);
-        System.out.println(name.hashCode());
-        System.out.println("Hello "+ name);
-        System.out.println(name.concat((" Shah")));
+        // String name = new String();
+        // name = "Vansh";
+        // System.out.println(name);
+        // System.out.println(name.hashCode());
+        // System.out.println("Hello "+ name);
+        // System.out.println(name.concat((" Shah")));
+
+
+        String name = "Vansh";
+        name = name + " Shah";
+        System.out.println(("Hello "+ name));
+
+        String s1 = "Vansh";
+        String s2 = "Vansh";
+
+        System.out.println(s1==s2);
         
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+String name = new String();
+        // name = "Vansh";
+        // System.out.println(name);
+        // System.out.println(name.hashCode());
+        // System.out.println("Hello "+ name);
+        // System.out.println(name.concat((" Shah")));
