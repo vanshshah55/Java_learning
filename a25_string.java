a42c9ed98125ca19bd9908671e76a0fbca7d47f8
Vanshshah55 <vips.vansh@gmail.com>
2025-10-10 01:08:03 +0530

    Fourth session practice codes

---
+++ a25_string.java
@@ -0,0 +1,11 @@
+public class a25_string {
+    public static void main(String[] a){
+        String name = new String();
+        name = "Vansh";
+        System.out.println(name);
+        System.out.println(name.hashCode());
+        System.out.println("Hello "+ name);
+        System.out.println(name.concat((" Shah")));
+        
+    }
+}
