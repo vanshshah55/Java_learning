305bb834fb2b25a61de3cd9e176ac26ad8d259ae
Vanshshah55 <vips.vansh@gmail.com>
2025-10-10 19:59:13 +0530

    Fifth session practice codes

---
+++ a30_encapsulation.java
@@ -0,0 +1,53 @@
+class Human{
+    private int age;
+    private String name;
+
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+
+    //USING SOURCE ACTION SETTING 
+
+    // public int getAge() {
+    //     return age;
+    // }
+    // public void setAge(int age) {
+    //     this.age = age;
+    // }
+    // public String getName() {
+    //     return name;
+    // }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+
+    
+
+}
+
+public class a30_encapsulation {
+    public static void main(String a[]){
+        Human obj1 = new Human();
+        obj1.setAge(20);
+        obj1.getAge();
+        obj1.setName("Vansh");
+        obj1.getName();
+
+        System.out.println(obj1.getName()+" : "+obj1.getAge());
+
+    }
+}
