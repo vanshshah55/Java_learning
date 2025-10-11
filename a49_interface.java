9e8b6671a6757a6d0e5ff0eb2d51511f1cc7ab5a
Vanshshah55 <vips.vansh@gmail.com>
2025-10-12 02:23:29 +0530

    Eighth session practice codes

--- a49_interface.java
+++ a49_interface.java
@@ -31,6 +31,15 @@ public static void main(String[] args) {
     developer vansh = new developer();
     vansh.devapp(obj);
     vansh.devapp(obj2);
+
+
+//     Types of Interface:-
+// 1. Normal interface
+// 	- an interface having two or more methods
+// 2. Functional interface (SAM)
+// 	- SAM => Single Abstract Method interface
+// 3. Marker interface
+// 	- an interface that as no methods (blank interface)
 }
 }
 
