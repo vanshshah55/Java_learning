c2e620fed792eab772dd867bf6fd218c833109bd
Vanshshah55 <vips.vansh@gmail.com>
2025-10-12 17:27:32 +0530

    Tenth session practice codes

--- a65_threads.java
+++ a65_threads.java
@@ -7,6 +7,15 @@
 // - Multiple threads can share resources.
 
 
+// - New State   
+// - Runnable State  ->  start() method
+// - Running State     -> a thead is running with run() method 
+// - Waiting State	    -> sleep(),  wait() method
+// - Dead State
+
+// Through notify(), you will go to waiting state to runnable state.
+// From Running, Runnable state to dead state through stop() method.
+
 
 
 // MULTIPLE THREADS 
