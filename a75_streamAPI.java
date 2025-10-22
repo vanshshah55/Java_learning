552051beb1ea7a61bb842ffae167e7915004b6d0
Vanshshah55 <vips.vansh@gmail.com>
2025-10-22 23:22:53 +0530

    Eleventh session practice codes

---
+++ a75_streamAPI.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class a75_streamAPI {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(5,3,4,2,1,7);
+
+        // Stream <Integer> s1 = nums.stream();
+        // //s1.forEach(n-> System.out.println(n));
+
+        // Stream <Integer> s2 = s1.filter(n->n%2==0);
+        // //s2.forEach(n->System.out.println(n));
+
+        // Stream<Integer> s3 = s2.map(n->n*2);
+        // //s3.forEach(n->System.out.println(n));
+
+        // int result=s3.reduce(0,(c,e)->c+e);
+        // System.out.println(result);
+
+        int result = nums.stream()
+                        .filter(n->n%2==0)
+                        .map(n->n*2)
+                        .reduce(0,(c,e)->c+e);
+        System.out.println(result);
+
+
+
+
+        
+    }
+}
